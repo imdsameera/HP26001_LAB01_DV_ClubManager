4e9984d66c0c2bc1c879df93c90468d080e87894
Sameera Lakshan <49205504+imdsameera@users.noreply.github.com>
2026-04-22 07:29:59 +0530

    repaired auth handle issue, added a inactivity sign-out system

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,5 +1,12 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import InactivityHandler from "@/components/auth/InactivityHandler";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <InactivityHandler />
+      {children}
+    </SessionProvider>
+  );
 }
